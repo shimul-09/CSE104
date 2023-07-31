ea452a4f4267a1eaccb3c911d5e3721da5c90e1c
shimul-09 <140424740+shimul-09@users.noreply.github.com>
2023-07-31 10:34:28 +0600

    lab problems

---
+++ lab3/New folder/fl.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    int i=0 ,ID=56;
+     for( ; ;) {
+     i=i+2;
+     printf("%d\n",i);
+   if (i>=ID) break;
+    }
+
+
+
+}
---
+++ lab3/New folder/for loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+  int i,j,k;
+ /* for( i=0, j=10; i < 100; i++)
+  printf("%d\n" , i+j);
+*/
+
+ /*for(i=0, j=10;i<100;i++)
+    printf("%d\n", i+j);
+*/
+for(i=5 , j=1; j<11; j++, k+=5,k<=50)
+ printf("%d*%d=%d\n", i,j,k=i*j);
+
+
+}
+
